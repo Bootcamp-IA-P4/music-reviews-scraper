609c006f28d5f73b6673fff4ac28451d09c822d6
Andrea Alonso <andreacor001@gmail.com>
2025-03-23 18:07:23 +0100

    feat: add layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* 🔥 Header fijo */}
+        <header className="fixed top-0 left-0 w-full border-b border-gray-200 bg-white z-50 text-center text-sm text-gray-600 py-4">
+          <Link href="/" className="text-gray-700 font-serif hover:text-black">
+            Pitchfork Review Recommender
+          </Link>
+        </header>
+        {/* 🚀 Main centrado, compensando el header fijo */}
+        <main className="max-w-5xl mx-auto px-8 mt-14 bg-white text-black font-serif">
+          {children}
+        </main>
+        {/* 📌 Footer opcional */}
+        <footer className="w-full border-t border-gray-200 p-4 text-center text-xs text-gray-500">
+          © {new Date().getFullYear()} Proyecto sin ánimo de lucro
+        </footer>{" "}
       </body>
     </html>
   );
